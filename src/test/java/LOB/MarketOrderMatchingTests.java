17bf899af6654665665f7a8328480557bf44e391
Miti Nopnirapath <mitip.2022@mqf.smu.edu.sg>
2023-06-19 21:19:28 +0800

    Added more testing functions. This time for how MarketOrder should interact with LimitOrderGroup

--- src/test/java/LOB/MarketOrderMatchingTests.java
+++ src/test/java/LOB/MarketOrderMatchingTests.java
@@ -4,5 +4,42 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MarketOrderMatchingTests {
+    LimitOrderGroup getTenBidOrderGroup(){
+        LimitOrderGroup priceGroup = new LimitOrderGroup(120.0, OrderEnum.Side.BID);
 
+        for (int i = 0; i < 10; ++i){
+            priceGroup.addOrder(new LimitOrder(120, 10., OrderEnum.Side.BID, 12));
+        }
+
+        return priceGroup;
+    }
+
+    @Test
+    public void testTakeOrder() {
+        LimitOrderGroup priceGroup = getTenBidOrderGroup();
+
+        MarketOrder marketOrder = new MarketOrder(25, OrderEnum.Side.ASK, 0);
+
+        priceGroup.takeOrder(marketOrder);
+        
+        Assert.assertEquals(marketOrder.amount, 0);
+        Assert.assertEquals(priceGroup.getPrice(), 120.0);
+        Assert.assertEquals(priceGroup.aggregateOrderAmount(), 75.0);
+        Assert.assertEquals(priceGroup.countOrder(), 8);
+        Assert.assertEquals(priceGroup.ordersDeque.peek().amount, 5.0);
+    }
+
+    @Test
+    public void testTakeBigOrder() {
+        LimitOrderGroup priceGroup = getTenBidOrderGroup();
+
+        MarketOrder marketOrder = new MarketOrder(105, OrderEnum.Side.ASK, 0);
+
+        priceGroup.takeOrder(marketOrder);
+        
+        Assert.assertEquals(marketOrder.amount, 5);
+        Assert.assertEquals(priceGroup.getPrice(), 120.0);
+        Assert.assertEquals(priceGroup.aggregateOrderAmount(), 0.0);
+        Assert.assertEquals(priceGroup.countOrder(), 0);
+    }
 }
